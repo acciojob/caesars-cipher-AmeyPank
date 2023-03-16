2515033f403bc0eb8a3ec5a246708b73b35a3764
Amey  Pankanti <86099941+AmeyPank@users.noreply.github.com>
2023-03-16 10:57:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 // Your Script here.
+function rot13(encryptedString){
+
+	var lookup = {
+		A: "N",
+		B: "O",
+		C: "P",
+		D: "Q",
+		E: "R",
+		F: "S",
+		G: "T",
+		H: "U",
+		I: "V",
+		J: "W",
+		K: "X",
+		L: "Y",
+		M: "Z",
+		N: "A",
+		O: "B",
+		P: "C",
+		Q: "D",
+		R: "E",
+		S: "F",
+		T: "G",
+		U: "H",
+		V: "I",
+		X: "J",
+		Y: "K",
+		Z: "L",
+		
+	}
+	var words = encryptedString.split(" ");
+	var decreptedWords = [];
+	for(var index = 0; index<words.length; index++){
+		var encryptedWord = words[i];
+		var decryptedWord = "";
+		for(var j = 0;j<encryptedWord.length; j++){
+			decreptedWord +=LOOKUP[encryptedWord.charAt(j)];
+		}
+		decreptedWords.push(decryptedWord)
+	}
+	return decreptedWords.join(" ");
+}
